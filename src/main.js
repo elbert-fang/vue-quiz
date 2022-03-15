b2381bf53b11a8ec068e56a3c82c80454141306e
elbert <elbert.fang@megaport.com>
2022-03-15 22:36:54 +1000

    vuex setup

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Vuex from 'vuex'
 import App from './App.vue'
 import EditPage from './pages/EditPage'
 import HomePage from './pages/HomePage'
 import QuizPage from './pages/QuizPage'
-
+import { store } from './store'
 Vue.use(VueRouter)
+Vue.use(Vuex)
 
 const router = new VueRouter({
   mode: 'history',
@@ -18,5 +20,6 @@ const router = new VueRouter({
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
