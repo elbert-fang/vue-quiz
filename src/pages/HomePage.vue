5c86a2705c6d2d2c3a0c69103e5962297ef6a9bf
Elbert Fang <elbert.fang@megaport.com>
2022-03-25 12:04:22 +1000

    add agify api call to Home

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,16 +1,34 @@
 <template>
   <div class="home-wrapper">
     <div class="content">
-      <h2> Vue Quiz </h2>
-      <p> Quiz Time! Firt thing first, what's your name? </p>
-      <p>Hi NAME, the system guessed you are XX years old. <br />
-        (It's not always accurate, don't be offened!😝)
-      </p>
-    </div>
+      <h2 class="heading"> Quiz Time! </h2>
 
-    <div class="nav">
-      <router-link to="/quiz">Start Quiz</router-link>
-      <router-link to="/edit">Edit Quiz</router-link>
+      <div class="flex flex-column">
+
+        <template v-if="!(name && age)">
+          <p> Firt thing first, what's your name? </p>
+          <input class="name-input full-width my-1"
+            id="name_input"
+            v-model="name"
+            placeholder="Type your name here" />
+          <button class=""
+            :disabled="loading"
+            @click="getAge">Look up your age</button>
+        </template>
+
+        <p class="full-width"
+          v-if="loading">Loading</p>
+        <template v-else>
+          <p v-if="errorMsg">{{ errorMsg }}</p>
+          <template v-if="name && age">
+            <p class="my-1">Hi {{name}}, the system guessed you are {{age}} years old. <br />
+              (It's not always accurate, don't be offened!😝)<br />
+              Anyway... Let's play the quiz game now!
+            </p>
+            <a href="/quiz"><button class="btn-start">Start Quiz</button></a>
+          </template>
+        </template>
+      </div>
     </div>
 
   </div>
@@ -18,6 +36,37 @@
 
 <script>
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+
+  data() {
+    return {
+      name: '',
+      age: 0,
+      errorMsg: '',
+      loading: false
+    }
+  },
+
+  methods: {
+    async getAge() {
+      const baseurl = 'https://api.agify.io?'
+      this.errorMsg = ''  // reset the error msg
+      this.loading = true
+
+      try {
+        const result = await fetch(`${baseurl}name=${this.name}`)
+        const json = await result.json()
+        this.age = json.age
+      } catch (error) {
+        this.errorMsg = 'There was an error, please try again'
+        this.age = 0
+      } finally {
+        this.loading = false
+      }
+
+
+    }
+  }
+
 }
 </script>
